5d9af73e430f6fcf980cdc6b9a4ae3fedc944a52
majavarsava <maja.varsava.4@gmail.com>
2024-11-06 09:35:06 +0100

    Dodana poruka s imenom

--- RUAP-Projekt/RUAP-Projekt/RUAP-Projekt.cpp
+++ RUAP-Projekt/RUAP-Projekt/RUAP-Projekt.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Pozdrav iz aplikacije od Maje Varsave";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
